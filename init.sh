643b177bbbfd1434732ba65278f5963742c6a84d
DGermes <vitaly2030@mail.ru>
2020-07-06 16:28:12 +0600

    75 Windows

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
   sudo rm -rf /etc/nginx/sites-enabled/default
 fi
 touch /home/box/nginx.log
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
+# sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
