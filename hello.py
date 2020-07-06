a7d28be6b8fd278338323cc2b8325fa1b703f9fb
DGermes <vitaly2030@mail.ru>
2020-07-06 19:32:52 +0600

    WSGI 1

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+
+def wsgi_application(environ, start_response):
+    result = []
+    result = '\r\n'.join(environ['QUERY_STRING'].split('&'))
+
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    # body = 'Hello, world!'
+    start_response(status, headers)
+    return iter([result])
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 # Run Bush
 
-# Nginx
+# Nginx - configuration
 # if [ -f /etc/nginx/sites-enabled/default ]; then
 #  sudo rm -rf /etc/nginx/sites-enabled/default
 # fi
@@ -9,3 +9,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 # sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# WSGI
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
