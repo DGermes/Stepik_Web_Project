3b9674b2610158f59606bc45cd3464be54226abd
DGermes <vitaly2030@mail.ru>
2020-07-06 16:32:37 +0600

    5 Windows

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 # Run Bush
 
 # Nginx
-if [ -f /etc/nginx/sites-enabled/default ]; then
-  sudo rm -rf /etc/nginx/sites-enabled/default
-fi
-touch /home/box/nginx.log
+# if [ -f /etc/nginx/sites-enabled/default ]; then
+#  sudo rm -rf /etc/nginx/sites-enabled/default
+# fi
+# touch /home/box/nginx.log
 # sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
