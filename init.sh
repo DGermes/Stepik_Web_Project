80c068b0513a036e873d80ef10399fbbe7ff009a
DGermes <vitaly2030@mail.ru>
2020-07-06 20:08:52 +0600

    WSGI 1

--- init.sh
+++ init.sh
@@ -7,9 +7,10 @@
 # touch /home/box/nginx.log
 # sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-# sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # WSGI
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
-sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/gunicorn restart
+sudo service gunicorn restart
