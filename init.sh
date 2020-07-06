56429647910c40aebcb0882da94014f1c6fe5840
DGermes <vitaly2030@mail.ru>
2020-07-06 16:39:50 +0600

    3 Windows

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@
 # fi
 # touch /home/box/nginx.log
 # sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+# sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
