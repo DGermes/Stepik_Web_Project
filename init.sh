5553d94554b7a593d5af5583a25bd79cf5ec8585
DGermes <vitaly2030@mail.ru>
2020-07-07 10:22:41 +0600

    WSGI 1

--- init.sh
+++ init.sh
@@ -14,6 +14,6 @@ sudo /etc/init.d/nginx restart
 # WSGI
 # sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 # sudo /etc/init.d/gunicorn restart
-# sudo service gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
+sudo service gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application
 # sudo gunicorn -bind='0.0.0.0:8080' hello:wsgi_application
