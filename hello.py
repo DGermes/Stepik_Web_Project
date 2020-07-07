e7c4fdbda167c517e386af0e1791d11a81b5bedb
DGermes <vitaly2030@mail.ru>
2020-07-07 09:44:21 +0600

    WSGI 1

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@
 
 def wsgi_application(environ, start_response):
     # result = '\r\n'.join(environ['QUERY_STRING'].split('&'))
-    result = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-
+    # result = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    result = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                    encoding="utf8")]
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     # body = 'Hello, world!'
