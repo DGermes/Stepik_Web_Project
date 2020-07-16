0a8337978c77c45baf303e4b8fd390c7ad218cf1
DGermes <vitaly2030@mail.ru>
2020-07-16 19:19:53 +0600

    ffh

--- init.sh
+++ init.sh
@@ -5,7 +5,6 @@
 #  sudo rm -rf /etc/nginx/sites-enabled/default
 # fi
 # touch /home/box/nginx.log
-# sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-available/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
@@ -15,10 +14,10 @@ sudo /etc/init.d/nginx restart
 # sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 # sudo /etc/init.d/gunicorn restart
 # sudo service gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
+#sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
 # sudo gunicorn -bind='0.0.0.0:8080' hello:wsgi_application
 
 #  ---- Django
 # sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
 # sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
-#     sudo gunicorn -b /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo gunicorn -b /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
