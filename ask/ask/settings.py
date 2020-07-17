eb14c186c932f8e679ef9b34c731b5e44fccea38
DGermes <vitaly2030@mail.ru>
2020-07-17 18:17:11 +0600

    ffh

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'ask',
-    'qa',
+    'ask.ask',
+    'ask.qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
